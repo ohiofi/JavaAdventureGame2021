ec0686aadfe529db26f13970a37da237d74c9144
Justin Riley <20hz20khz@gmail.com>
2021-10-14 10:04:01 -0400

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,19 @@
+/*
+Room
+ - NoArgsConstructor
+ - Room(String _name)
+ - getCharacter()
+ - getItem()
+ - getLocationTo(String direction)
+ - getName()
+ - getPossibleDirections()
+ - setCharacter(Npc character)
+ - setDescription(String d)
+ - setItem(Item i)
+ - setName(String _name)
+ - toString() // returns the description
+*/
+public class Room
+{
+  
+}
