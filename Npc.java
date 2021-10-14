7cfbed82e00848264473305c910dfba69026e49c
Justin Riley <20hz20khz@gmail.com>
2021-10-14 10:04:54 -0400

    Create Npc.java

---
+++ Npc.java
@@ -0,0 +1,15 @@
+/*
+Npc
+ - NoArgsConstructor
+ - Npc(String _name, String _description)
+ - getName
+ - getSpeech
+ - setDescription(String d)
+ - setName(String _name)
+ - setSpeech
+ - toString() // returns the description
+*/
+public class Npc
+{
+  
+}
