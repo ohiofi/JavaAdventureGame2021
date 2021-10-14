f34f37ba3b8c697234fd1cbd58ae2e51dc4edff6
Justin Riley <20hz20khz@gmail.com>
2021-10-14 10:16:51 -0400

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,17 @@
+/*
+Item
+ - NoArgsConstructor
+ - Item(String _name, String _description)
+ - getMagicType()
+ - getName()
+ - getStrength()
+ - setDescription(String d)
+ - setName(String _name)
+ - isBroken()
+ - toString() // returns the description
+ - weaken()
+ */
+public class Item
+{
+  
+}
